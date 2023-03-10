e9d772b09c67395ba712f175281b3e7d2e768bcf
timch12 <122532286+timch12@users.noreply.github.com>
2023-03-10 11:35:37 +0300

    day 4

--- day 2-3/DataAccess/Interfaces/IRepositoryBase.cs
+++ day 2-3/DataAccess/Interfaces/IRepositoryBase.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess.Interfaces
 {
-    internal interface IRepositoryBase
+    internal interface IRepositoryBase<T>
     {
+        IQueryable<T> FindaLL();
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+
     }
 }
